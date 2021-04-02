782d1d1b7e0a5c1431bc4266609f052bd7c977bb
Bairon Freire <bairon.freire@iteria.com.co>
2021-04-02 14:05:24 -0500

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,45 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './auth/login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { GraphComponent } from './pages/graph/graph.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { SpinnerComponent } from './shared/spinner/spinner.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 import { APP_ROUTES } from './app.routes';
-import { PagesComponent } from './pages/pages.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { AuthComponent } from './auth/auth.component';
+import { PagesModule } from './pages/pages.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    GraphComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    SpinnerComponent,
-    PagesComponent,
-    RegisterComponent,
-    AuthComponent,
   ],
-  exports: [
-    SpinnerComponent
-  ],
+  exports: [],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,34 +1,12 @@
 import { RouterModule, Routes } from "@angular/router";
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+
 import { LoginComponent } from './auth/login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { GraphComponent } from './pages/graph/graph.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { AuthComponent } from './auth/auth.component';
 
-const appRoutes: Routes = [
-    { 
-        path: '', 
-        component: PagesComponent,
-        children: [
-            { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-            { path: 'dashboard', component: DashboardComponent},
-            { path: 'progress', component: ProgressComponent},
-            { path: 'graph', component: GraphComponent},
-        ]
-    },
-    { 
-        path: 'auth', 
-        component: AuthComponent,
-        children: [
-            { path: '', redirectTo: '/auth/login', pathMatch: 'full'},
-            { path: 'login', component: LoginComponent},
-            { path: 'register', component: RegisterComponent},
-        ]
-    },
-  
+const appRoutes: Routes = [  
     { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
     { path: '**', component: NopagefoundComponent},
 ];
